13694e845ec9c87ed31c55e173d9d74ca054cadf
Boutainaelatbaoui <boutaina.elatbaoui1998@gmail.com>
2022-12-25 19:46:13 +0100

    Modified main.js and index.js and data.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,21 @@ let answer_2 = document.getElementById("answer-2");
 let answer_3 = document.getElementById("answer-3");
 let answer_4 = document.getElementById("answer-4");
 
-let question   = document.getElementById("quiz");
-let n_question = document.getElementById("question-number");
-let answer     = document.querySelector("answer");
+let question    = document.getElementById("quiz");
+let n_question  = document.getElementById("question-number");
+let answer_list = Array.from(document.querySelectorAll(".answer"));
+let progress    = document.getElementById("progress");
+// console.log(answer_list);
 
 let start = document.getElementById("start");
 let next = document.getElementById("next");
 
-let index = Math.floor(Math.random() * questions.length);
+let array = [];
+let index;
 
-//Total points
-let correct = 0;
+let question_count = 0;
+let score = 0;
+let count = 0;
 
 //Store User Answer
 let user_answer = undefined;
@@ -26,9 +30,7 @@ answer_2.addEventListener("click", nextQuiz);
 answer_3.addEventListener("click", nextQuiz);
 answer_4.addEventListener("click", nextQuiz);
 
-function startQuiz(e){
-    e.preventDefault();
-
+function startQuiz(){
     let username = document.getElementById("username-input").value;
     document.getElementById("username").innerText = `Hello ${username}`;
     
@@ -37,19 +39,16 @@ function startQuiz(e){
         document.getElementById("quiz-app").style.display     = "flex";
         document.getElementById("quiz-info").style.display    = "block";
         document.getElementById("container").style.marginTop  = "80px";
-        loadData();
+        randomQuestion();
     }
     else{
         document.getElementById("empty-input").innerText = "Please Enter a name to start the quizz";
     }
 }
 
-function nextQuiz(e){
-    e.preventDefault();
-
+function nextQuiz(){
     document.getElementById("next").style.display    = "block";
     document.getElementById("answers").style.marginBottom  = "15px";
-
 }
 
 function nextButton(){
@@ -61,23 +60,46 @@ function nextQuestion(){
     document.getElementById("next").style.display    = "none";
     document.getElementById("answers").style.marginBottom  = "0";
     document.getElementById("chevron").style.display    = "none";
-    loadData();
+    if (question_count == questions.length - 1) {
+        document.getElementById("first-slide").style.display  = "block";
+        document.getElementById("quiz-app").style.display     = "none";
+        document.getElementById("quiz-info").style.display    = "none";
+    } else {
+        question_count++;
+        randomQuestion();
+    }
 }
 
-function loadData(){
-    if(index > questions.length -1){
-        console.log("Hello");
+function show() {
+    question.innerText = questions[index]['quest'];
+    for (let i = 0; i< answer_list.length; i++) {
+        answer_list[i].innerText = questions[index]['option'][i];
     }
-    else{
-        question.innerText   = questions[index].quest;
-        answer_1.innerText   = questions[index].answer1;
-        answer_2.innerText   = questions[index].answer2;
-        answer_3.innerText   = questions[index].answer3;
-        answer_4.innerText   = questions[index].answer4;
-        index = Math.floor(Math.random() * questions.length);
-        console.log(index);
+    count++;
+    progress.innerText = `${count} questions`;
+    console.log(count);
+}
+
+function randomQuestion(){
+    let random_number = Math.floor(Math.random() * questions.length);
+    if(array.length > 0){
+        if (array.includes(random_number)) {
+            randomQuestion();
+        } else {
+            index = random_number;
+            show();
+            array.push(index);
+        }
+    }
+    if (array.length == 0) {
+        index = random_number;
+        show();
+        array.push(index);
     }
 }
+
+
+
 
 
 
