4dffe191c97ba09ec5b2b295ff8bbe294f016139
Boutainaelatbaoui <boutaina.elatbaoui1998@gmail.com>
2022-12-25 21:18:04 +0100

    Added a function which verified is the selected option correct or not

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-let answer_1 = document.getElementById("answer-1");
-let answer_2 = document.getElementById("answer-2");
-let answer_3 = document.getElementById("answer-3");
-let answer_4 = document.getElementById("answer-4");
-
 let question    = document.getElementById("quiz");
 let n_question  = document.getElementById("question-number");
 let answer_list = Array.from(document.querySelectorAll(".answer"));
@@ -17,18 +12,13 @@ let index;
 
 let question_count = 0;
 let score = 0;
+let correct = 0;
+let wrong = 0;
 let count = 0;
-
-//Store User Answer
-let user_answer = undefined;
 
 start.addEventListener("click", startQuiz);
 next.addEventListener("mouseover", nextButton);
 next.addEventListener("click", nextQuestion);
-answer_1.addEventListener("click", nextQuiz);
-answer_2.addEventListener("click", nextQuiz);
-answer_3.addEventListener("click", nextQuiz);
-answer_4.addEventListener("click", nextQuiz);
 
 function startQuiz(){
     let username = document.getElementById("username-input").value;
@@ -46,11 +36,6 @@ function startQuiz(){
     }
 }
 
-function nextQuiz(){
-    document.getElementById("next").style.display    = "block";
-    document.getElementById("answers").style.marginBottom  = "15px";
-}
-
 function nextButton(){
 
     document.getElementById("chevron").style.display    = "inline";
@@ -60,6 +45,13 @@ function nextQuestion(){
     document.getElementById("next").style.display    = "none";
     document.getElementById("answers").style.marginBottom  = "0";
     document.getElementById("chevron").style.display    = "none";
+
+    for (let i = 0; i < answer_list.length; i++){
+        answer_list[i].classList.remove("correct");
+        answer_list[i].classList.remove("wrong");
+        answer_list[i].disabled = false; 
+    }
+
     if (question_count == questions.length - 1) {
         document.getElementById("first-slide").style.display  = "block";
         document.getElementById("quiz-app").style.display     = "none";
@@ -72,7 +64,7 @@ function nextQuestion(){
 
 function show() {
     question.innerText = questions[index]['quest'];
-    for (let i = 0; i< answer_list.length; i++) {
+    for (let i = 0; i < answer_list.length; i++) {
         answer_list[i].innerText = questions[index]['option'][i];
     }
     count++;
@@ -96,6 +88,33 @@ function randomQuestion(){
         show();
         array.push(index);
     }
+}
+
+for (let i = 0; i < answer_list.length; i++) {
+    answer_list[i].addEventListener("click", () => {
+
+        let id = answer_list[i].getAttribute("data-id");
+
+        if(id == questions[index].response){
+            answer_list[i].classList.add("correct");
+            score+=10;
+            console.log("score: " + score);
+            correct++;
+            console.log("correct: " + correct);
+        }
+        else{
+            answer_list[i].classList.add("wrong");
+            console.log("score: " + score);
+            wrong++;
+            console.log("wrong: " + wrong);
+        }
+        for (let j = 0; j < answer_list.length; j++) {
+            answer_list[j].disabled = true;   
+        }
+        document.getElementById("next").style.display    = "block";
+        document.getElementById("answers").style.marginBottom  = "15px";
+    })
+    
 }
 
 
